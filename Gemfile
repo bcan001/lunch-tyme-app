6fedc62e83d3311e7fc8e80ec571001e628fe274
bcaneba <bcaneba@gmail.com>
2017-03-29 23:15:09 -0500

    added devise gem for authentication

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ gem 'geocoder'
 gem 'gmaps4rails'
 gem 'underscore-rails'
 
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    restaurants_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+
   def remote_ip
     if Rails.env.production?
 		  request.remote_ip
