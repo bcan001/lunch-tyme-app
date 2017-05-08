fff20d4f7d0c256b46bfb8ffd70731ec0c3b27e9
bcaneba <bcaneba@gmail.com>
2017-05-07 21:45:26 -0500

    added pagination to view.

--- app/assets/javascripts/components/restaurant_of_the_day_list.jsx
+++ app/assets/javascripts/components/restaurant_of_the_day_list.jsx
@@ -1,7 +1,7 @@
 var RestaurantOfTheDayList = React.createClass({
   render: function () {
     var suggestedRestaurantNodes = this.props.suggested_restaurants.map(function ( restaurant, i ) {
-      return <RestaurantOfTheDay key={i} name={ restaurant.name } phone= { restaurant.phone } />
+      return <RestaurantOfTheDay key={i} name={ restaurant.name } phone= { restaurant.phone } image_url={ restaurant.image_url } rating_img_url_small={ restaurant.rating_img_url_small } />
     });
 
     return (
